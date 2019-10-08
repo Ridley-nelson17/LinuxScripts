c12a758bc0f278a986893d26b8d35065bb8f2dd0
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-10-08 11:08:04 -0500

    Create Basic_commands.sh

---
+++ Basic_commands.sh
@@ -0,0 +1,16 @@
+# Auto-removes useless files within a package
+sudo apt-get autoremove <Package>
+
+# Prints the working directoryy
+pwd
+
+# Self explanitory
+history
+
+# (display filesystem) displays information about the disk space usage of all mounted filesystems
+df
+
+# (directory usage) command displays the size of a directory and all of its subdirectories.
+du
+
+free
